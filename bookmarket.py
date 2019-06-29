32215b097bfe8ee7e059086ee2e937097d149871
ccty123 <36689055+ccty123@users.noreply.github.com>
2019-06-30 02:26:17 +0800

    Add files via upload
    DB operation

--- bookmarket.py
+++ bookmarket.py
@@ -45,11 +45,14 @@ class BookMarket(object):
 
     def createUser(self,user_info):
         '''这是一个注册操作，返回一个整数'''
+        ''''''
         try:
             split_info = user_info.split('&')
-            new_name = split_info[0][9:]
+            new_id = split_info[0][9:]
             new_password = split_info[1][9:]
             new_email = split_info[2][6:]
+            new_name = split_info[3][5:]
+            new_telphone = split_info[4][9:]
         except:
             print('Transfer Data Error!')
             return 1
@@ -86,9 +89,8 @@ class BookMarket(object):
             print('create user successful')
         except Exception as e:
             print(e)
-
         return 0
-    
+
     def login(self, user_info):
         '''这个是登录操作,返回一个布尔值'''
         #  username=zzz&password=123 取出名字和密码
@@ -114,12 +116,12 @@ class BookMarket(object):
 
     def get_book_data(self):
         '''这是一个遍历书籍操作'''
-        print('show products')
+        print('all books in store')
         try:
             sql = '''select * from book'''
             ret = self.cur.execute(sql)
+            data_str = ''
             if ret:
-                data_str = ''
                 rows = self.cur.fetchall()
                 for row in rows:
                     for i in row:
@@ -127,13 +129,16 @@ class BookMarket(object):
                         data_str += '&'
                     data_str += '|'
                 print(data_str)
-                return data_str
+            return data_str
         except Exception as e:
             print(e)
             
     def select_book_data(self,data_info):
         '''这是一个查询书籍操作'''
-        print('show products')
+        print('select book in store')
+        if data_info == '':
+            return ''
+
         try:
             s = data_info.split('&')
             sql = '''select * from book where '''
@@ -141,8 +146,8 @@ class BookMarket(object):
             for tr in s[1:]:
                 sql = sql + ''' and ''' + tr
             ret = self.cur.execute(sql)
+            data_str = ''
             if ret:
-                data_str = ''
                 rows = self.cur.fetchall()
                 for row in rows:
                     for i in row:
@@ -150,31 +155,32 @@ class BookMarket(object):
                         data_str += '&'
                     data_str += '|'
                 print(data_str)
-                return data_str
+            return data_str
         except Exception as e:
             print(e)
     
     def add_book_data(self,book_info):
         '''这是一个添加书籍操作'''
-        print('add products')
+        print('insert book')
         try:
             split_info = book_info.split('&')
             new_id = split_info[0][3:]
             new_name = book_info[1][5:]
-            new_seilor = split_info[2][6:]
-            new_beforeprice = float(split_info[3][11:])
-            new_nowprice = float(book_info[4][8:])
-            new_category = split_info[5][8:]
-            new_introduction = split_info[6][12:]
-            new_storm = split_info[7][6:]
+            new_seil = split_info[2][5:]
+            new_beforeprice = float(split_info[3][12:])
+            new_nowprice = float(book_info[4][9:])
+            new_category = int(split_info[5][9:])
+            new_storm = int(split_info[6][6:])
+            new_images = split_info[7][7:]
+            new_introduction = split_info[8][13:]
         except:
             print('Transfer Data Error!')
             return False
         
-        sql = '''insert book (id,name,seilor,beforeprice,nowprice,category,introduction,storm) 
-                    values(%s,%s,%s,%f,%f,%s,%s,%s)'''
+        sql = '''insert book (id,name,seil,beforeprice,nowprice,category,storm,images,introduction) 
+                    values(%s,%s,%s,%f,%f,%d,%d,%s,%s)'''
         try:
-            ret = self.cur.execute(sql,[new_id,new_name,new_seilor,new_beforeprice,new_nowprice,new_category,new_introduction,new_storm])
+            ret = self.cur.execute(sql,[new_id,new_name,new_seil,new_beforeprice,new_nowprice,new_category,new_storm,new_images,new_introduction])
         except Exception as e:
             print(e)
         print('add book successful')
@@ -182,27 +188,27 @@ class BookMarket(object):
 
     def add_online_order(self,order_info):
         '''这是一个添加订单操作'''
-        print('add order')
+        print('insert order')
         try:
             split_info = order_info.split('&')
             new_id = split_info[0][3:]
-            new_name = book_info[1][5:]
-            new_bookmaster = split_info[2][10:]
-            new_bookaccept = split_info[3][10:]
-            new_num = int(book_info[4][3:])
-            new_price = float(split_info[5][6:])
-            new_destination = split_info[6][11:]
-            new_mastertel = split_info[7][9:]
-            new_accepttel = split_info[8][9:]
+            new_bookname = book_info[1][9:]
+            new_seil = split_info[2][5:]
+            new_seiltel = split_info[3][8:]
+            new_buy = split_info[4][4:]
+            new_buytel = split_info[5][7:]
+            new_num = int(book_info[6][4:])
+            new_price = float(split_info[7][6:])
+            new_destination = split_info[8][12:]
             new_state = int(split_info[9][5:])
         except:
             print('Transfer Data Error!')
             return False
         
-        sql = '''insert book (id,name,bookmaster,bookaccept,num,price,destination,mastertel,accepttel,state) 
-                    values(%s,%s,%s,%s,%d,%f,%s,%s,%s,%d)'''
+        sql = '''insert book (id,bookname,seil,seiltel,buy,buytel,num,price,destination,state) 
+                    values(%s,%s,%s,%s,%s,%s,%d,%f,%s,%d)'''
         try:
-            ret = self.cur.execute(sql,[new_id,new_name,new_bookmaster,new_bookaccept,new_num,new_price,new_destination,new_mastertel,new_accepttel,new_state])
+            ret = self.cur.execute(sql,[new_id,new_bookname,new_seil,new_seiltel,new_buy,new_buytel,new_num,new_price,new_destination,new_state])
         except Exception as e:
             print(e)
         print('add order successful')
@@ -210,9 +216,9 @@ class BookMarket(object):
 
     def update_online_order(self,order_state,order_id):
         '''这是一个更新订单状态操作'''
-        print('show products')
+        print('update order state')
         try:
-            sql = '''update online_order set state = %s where id = %s'''
+            sql = '''update online_order set state = %d where id = %s'''
             ret = self.cur.execute(sql,[order_state,order_id])
         except Exception as e:
             print(e)
@@ -220,14 +226,14 @@ class BookMarket(object):
         print('update state successful')
         return True
 
-    def get_online_order(self,order_info):
+    def get_online_order(self,user_name):
         '''这是一个遍历个人订单操作'''
         print('show products')
         try:
-            sql = '''select * from book where bookaccept=%s'''
-            ret = self.cur.execute(sql,[order_info,])
+            sql = '''select * from online_order where buy=%s'''
+            ret = self.cur.execute(sql,[user_name,])
+            data_str = ''
             if ret:
-                data_str = ''
                 rows = self.cur.fetchall()
                 for row in rows:
                     for i in row:
@@ -235,7 +241,7 @@ class BookMarket(object):
                         data_str += '&'
                     data_str += '|'
                 print(data_str)
-                return data_str
+            return data_str
         except Exception as e:
             print(e)
 
