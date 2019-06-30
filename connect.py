5c5bf924a097e20b26465352bd2f8885999f3f08
SummerOfMe <52004040+SummerOfMe@users.noreply.github.com>
2019-06-30 17:22:47 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,5 +1,3 @@
-#encoding=utf-8
-import urllib
 import BaseHTTPServer
 import SimpleHTTPServer
 import SocketServer
@@ -8,11 +6,11 @@ import shutil
 import json
 from bookmarket import BookMarket
 def userReg(info):
-    username=info.getvalue('username')
-    password=info.getvalue('password')
-    email=info.getvalue('email')
-    name=info.getvalue('name')
-    telphone=info.getvalue('telphone')
+    username=info['username']
+    password=info['password']
+    email=info['email']
+    name=info.getvalue('name']
+    telphone=info['telphone']
     reginfo=bk.createUser('username='+username+'&password='+password+'&email='+email+'&name='+name+'&telphone='+telphone)
     if reginfo==0:
         regreturn={'success':'true','maindata':{'username':username,'password':password}}
@@ -28,22 +26,22 @@ def userReg(info):
         regreturn={'success':'false','maindata':'namerepeat'}
     else:
         regreturn={'success':'false','maindata':'emailrepeat'}
-
+        
     return json.dumps(regreturn)
 
 def userLogin(info):
-    username=info.getvalue('username')
-    password=info.getvalue('password')
+    username=info['username']
+    password=info['password']
     loginfo=bk.login('username='+username+'&password='+password)
     if loginfo==True:
         logreturn={'success':'true','maindata':{'username':username,'password':password}}
     else:
         logreturn={'success':'false','maindata':'nameorpsw'}
-
+        
     return json.dumps(logreturn)
-
+            
 def bookSearch(info):
-    bookname=info.getvalue('bookname')#需修改，可能不是书名
+    bookname=info['bookname']#需修改，可能不是书名
     bsinfo=bk.get_book_data('bookname='+bookname)
     if bsinfo:
         allbook=bsinfo.split('|')
@@ -56,51 +54,51 @@ def bookSearch(info):
         bsreturn={'success':'true','maindata':bookdir}
     else:
         bsreturn={'success':'false','maindata':''}
-
+    
     return json.dumps(bsreturn,ensure_ascii=False)
 
 def bookAdd(self,info):
-    bookid=info.getvalue("id")
-    bookname=info.getvalue("name")
-    booksel=info.getvalue("sell")
-    bookbp=info.getvalue("beforeprice")
-    booknp=info.getvalue("nowprice")
-    bookcate=info.getvalue("category")
-    bookintr=info.getvalue("introduction")
-    booksto=info.getvalue("store")
-    bookima=info.getvalue("images")
+    bookid=info["id"]
+    bookname=info["name"]
+    booksel=info["sell"]
+    bookbp=info["beforeprice"]
+    booknp=info["nowprice"]
+    bookcate=info["category"]
+    bookintr=info["introduction"]
+    booksto=info["store"]
+    bookima=info["images"]
     bookinf='id='+bookid+'&name='+bookname+'&seil='+booksel+'&beforeprice='+bookbp+'&nowprice='+booknp+'&category='+bookcate+'&storm='+booksto+'&images='+bookima+'&introduction='+bookintr
     boaddinfo=bk.add_book_data(bookinf)
-
+    
     if boaddinfo==True:
         boaddreturn={'success':'true','maindata':''}
     else:
         boaddreturn={'success':'false','maindata':'dataformerror'}
-
+        
     return json.dumps(boaddreturn)
-
+            
 def orderAdd(self,info):
-    orderid=info.getvalue("id")
-    orderbook=info.getvalue("bookname")
-    ordersel=info.getvalue("sell")
-    orderseltel=info.getvalue("selltel")
-    orderbuy=info.getvalue("buy")
-    orderbuytel=info.getvalue("buytel")
-    ordernum=info.getvalue("num")
-    orderpri=info.getvalue("price")
-    orderdes=info.getvalue("destination")
-    ordersta=info.getvalue("state")
+    orderid=info["id"]
+    orderbook=info["bookname"]
+    ordersel=info["sell"]
+    orderseltel=info["selltel"]
+    orderbuy=info["buy"]
+    orderbuytel=info["buytel"]
+    ordernum=info["num"]
+    orderpri=info["price"]
+    orderdes=info["destination"]
+    ordersta=info["state"]
     orderinfo='id='+orderid+'&bookname='+orderbook+'&seil='+ordersel+'&seiltel='+orderseltel+'&buy='+orderbuy+'&buytel='+orderbuytel+'&num='+ordernum+'&price='+orderpri+'&destination='+orderdes+'&state='+ordersta
     oraddinfo=bk.add_online_order(orderinf)
     if oraddinfo==False:
         oraddreturn={'success':'false','maindata':'dataformerror'}
     else:
         oraddreturn={'success':'true','maindata':''}
-
+        
     return json.dumps(oraddreturn)
 
 def orderSearch(self,info):
-    bookaccept=info.getvalue('bookaccept')
+    bookaccept=info['buy']
     bsinfo=bk.get_online_order(bookaccept)
     if bainfo:
         allaccept=bsinfo.split('|')
@@ -113,13 +111,13 @@ def orderSearch(self,info):
         bsreturn={'success':'true','maindata':orderdir}
     else:
         bsreturn={'success':'false','maindata':''}
-
+    
     return json.dumps(bsreturn,ensure_ascii=False)
-
+        
 
 def orderUpdate(self,info):
-    orderid=info.getvalue('id')
-    orderstate=info.getvalue('state')
+    orderid=info['id']
+    orderstate=info['state']
     orupinfo=bk.update_online_order(orderstate,orderid)
     if orupinfo==False:
         orupreturn={'success':'false','maindata':'exceptionoccurs'}
@@ -127,25 +125,31 @@ def orderUpdate(self,info):
         orupreturn={'success':'true','maindata':''}
     return json.dumps(orupreturn)
 
-class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler): 
     def do_OPTIONS(self):
-        self.send_response(200)
+        self.send_response(200)   
         self.send_header("Access-Control-Allow-Origin","*")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type")
-    def do_GET(self):
+    def do_GET(self): 
         mpath,margs=urllib.splitquery(self.path) # ?分割
         if margs==None:
             SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
         else:
-            self.do_action(mpath, margs)
-    def do_POST(self):
+            self.do_action(mpath, margs) 
+            
+    def do_POST(self): 
         mpath,margs=urllib.splitquery(self.path)
-        datas = cgi.FieldStorage(fp = self.rfile,headers = self.headers,environ = {'REQUEST_METHOD': 'POST'})
-        self.do_action(mpath, datas)
+       # datas = cgi.FieldStorage(fp = self.rfile,headers = self.headers,environ = {'REQUEST_METHOD': 'POST'})
+        datas = self.rfile.read(int(self.headers['content-length']))
+        jsondatas=json.load(datas)
+        self.do_action(mpath, jsondatas)
+        #line = self.rfile.readline()
+      #  self.do_action(mpath, line)
 
+        
     def do_action(self, path, args):
-        operation=args.getvalue('operation')
+        operation=args['operation']
         if operation=='login':
             sendata=userLogin(args)
         elif operation=='register':
@@ -160,25 +164,28 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             sendata=bookSearch(args)
         elif operation=='updateoder':
             sendata=orderUpdate(args)
+        self.outputtxt(sendata)
+            '''
         dh = dataHandler()
         result = dh.run(path, sendata)
         self.outputtxt(result)
-
+        '''
+        
     def outputtxt(self, content):
         enc = "UTF-8"  #指定返回编码
-        content = content.encode(enc)
+        content = content.encode(enc)               
         f = io.BytesIO()
         f.write(content)
-        f.seek(0)
-        self.send_response(200)
+        f.seek(0)  
+        self.send_response(200)  
         self.send_header("Access-Control-Allow-Origin","*")
-        self.send_header("Content-type", "text/html; charset=%s" % enc)
+        self.send_header("Content-type", "text/html; charset=%s" % enc)  
         self.send_header("Content-Length", str(len(content)))
-        self.end_headers()
+        self.end_headers()  
         shutil.copyfileobj(f,self.wfile)
         f.close()
-        #self.wfile.write(content)
-
-bk = BookMarket()
-httpd = SocketServer.TCPServer(("127.0.0.1", 3000), ServerHandler)
-httpd.serve_forever()
+        #self.wfile.write(content)            
+ 
+bk = BookMarket()           
+httpd = SocketServer.TCPServer(("127.0.0.1", 3000), ServerHandler) 
+httpd.serve_forever() 
