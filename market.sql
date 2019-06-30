ebe4ba002d153f377285e6d11b30d64ddec47f82
ccty123 <36689055+ccty123@users.noreply.github.com>
2019-06-30 17:30:33 +0800

    Add files via upload
    sql脚本

--- market.sql
+++ market.sql
@@ -22,7 +22,7 @@
 drop database if exists bookmarket;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-create database bookmarket;
+create database bookmarket character set utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 use bookmarket;
 
@@ -39,20 +39,23 @@ create table user(
   email varchar(20) not null,
   name varchar(20) not null,
   telphone varchar(20) not null,
+  images varchar(20) not null default 'users.jpg',
   primary key(id),
   unique key idname(id),
   unique key emailname(email),
   unique key tel(telphone)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+alter table user MODIFY COLUMN name varchar(20) character set utf8;
+/*!40101 SET character_set = utf8 */;
 
 --
 -- Dumping data for table user
 --
 
-insert into user values('sellers','88889999','for@qq.com','陆小朋','18868113423');
+insert into user values('sellers','88889999','for@qq.com','陆小朋','18868113423','luxiaopeng.jpg');
 /*!40000 ALTER TABLE user ENABLE KEYS */;
-insert into user values('administrator','76214573','31@zju.edu.cn','方大同','18868108927');
+insert into user values('administrator','76214573','31@zju.edu.cn','方大同','18868108927','fangdatong.jpg');
 /*!40000 ALTER TABLE user ENABLE KEYS */;
 
 --
@@ -70,23 +73,28 @@ create table book(
   nowprice float not null,
   category int not null,
   storm int not null,
-  introduction varchar(500),
+  images varchar(20) not null,
+  introduction varchar(500) character set utf8,
   primary key(id),
   unique key idname(id),
   unique key bookname(name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+alter table book MODIFY COLUMN name varchar(20) character set utf8;
+alter table book MODIFY COLUMN seil varchar(20) character set utf8;
+alter table book MODIFY COLUMN introduction varchar(500) character set utf8;
+/*!40101 SET character_set = utf8 */;
 
 --
 -- Dumping data for table book
 --
 
 insert into book values(
-  'AKTH2134','论语','陆小朋',24.9,20.9,2,3,
+  'AKTH2134','论语','陆小朋',24.9,20.9,2,3,'lunyu.jpg',
   '《论语》是孔子及其弟子的语录结集，由孔子弟子及再传弟子编写而成，至战国前期成书。全书共20篇492章，以语录体为主，叙事体为辅，主要记录孔子及其弟子的言行，较为集中地体现了孔子的政治主张、伦理思想、道德观念及教育原则等。此书是儒家学派的经典著作之一，与《大学》《中庸》《孟子》并称“四书”，再加上《诗经》《尚书》《礼记》《周易》《春秋》，总称“四书五经”。');
 /*!40000 ALTER TABLE book ENABLE KEYS */;
 insert into book values(
-  'ATMN3672','巴黎圣母院','方大同',25.2,20.9,1,1,
+  'ATMN3672','巴黎圣母院','方大同',25.2,20.9,1,1,'paris.jpg',
   '《巴黎圣母院》以离奇和对比手法写了一个发生在15世纪法国的故事：巴黎圣母院副主教克罗德道貌岸然、蛇蝎心肠，先爱后恨，迫害吉ト赛女郎埃斯梅拉达。面目丑陋、心地善良的敲钟人卡西莫多为救女郎舍身。小说揭露了宗教的虚伪，宣告禁欲主义的破产，歌颂了下层劳动人民的善良、友爱、舍己为人，反映了雨果的人道主义思想。');
 /*!40000 ALTER TABLE book ENABLE KEYS */;
 
@@ -112,7 +120,11 @@ create table online_order(
   unique key idname(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
+alter table online_order MODIFY COLUMN bookname varchar(20) character set utf8;
+alter table online_order MODIFY COLUMN seil varchar(20) character set utf8;
+alter table online_order MODIFY COLUMN buy varchar(20) character set utf8;
+alter table online_order MODIFY COLUMN destination varchar(100) character set utf8 default '';
+/*!40101 SET character_set = utf8 */;
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
